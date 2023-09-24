30ad353534c14778d7b557e44263db99082b8897
ATHULARIGA <athulariga12@gmail.com>
2023-09-24 22:01:17 +0530

    Create calling_class_inside_a_class.py

---
+++ Python/Harish/calling_class_inside_a_class.py
@@ -0,0 +1,17 @@
+class Radio:
+    def turnOn(self,x):
+        if(x==1):
+            print("Radio is on ")
+        else:
+            print("Radio is off ")
+
+class Car:
+    def __init__(self,min,max) :
+        self.min = min
+        self.max = max
+        self.r=Radio()
+c1=Car(60,120)
+print(c1.min,c1.max)
+
+c1.r.turnOn(1)
+c1.r.turnOn(0)
