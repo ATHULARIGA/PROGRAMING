cdaf76cf5cb2ecf671d641c494126e63aa4ad643
ATHULARIGA <athulariga12@gmail.com>
2023-09-24 22:09:52 +0530

    Create reverse_str_without_reversing_full_stmt.py

---
+++ Python/kiran/string/reverse_str_without_reversing_full_stmt.py
@@ -0,0 +1,27 @@
+# using list
+# str=input("enter your string:")
+# li=str.split(" ")
+# for i in li:
+#     print(i[::-1],end=" ")
+
+
+
+str="India is my home"                                      #input("enter your string:")
+def rev_str(str):
+    new_str=""
+    count=0
+    for i in range(len(str)):
+        if (str[i]==" "):
+            str1=str[count:i+1][::-1]
+            new_str+=str1
+            count=i+1
+        elif i==len(str)-1:
+            str1=str[count:i+1][::-1]
+            new_str+=str1
+    print(new_str) 
+       
+
+rev_str(str)           
+
+
+ 
