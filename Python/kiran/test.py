26bb39c7d19dbae1e2ef576c4fead95bc8f4de7f
ATHULARIGA <athulariga12@gmail.com>
2023-09-24 22:10:05 +0530

    Delete test.py

--- Python/kiran/test.py
+++
@@ -1,9 +0,0 @@
-import math
-def factor(num):
-    for i in range(1,int(math.sqrt(num))+1):
-        if num%i==0:
-            print(i,end=" ")
-    for i in range(int(math.sqrt(num)),0,-1):
-        #if(i!=num//i):
-                print(num//i,end=" ")
-factor(40)
