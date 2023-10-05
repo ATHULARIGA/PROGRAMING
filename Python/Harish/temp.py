9bb7736f6384564c8a79305308db819edde62fe9
ATHULARIGA <athulariga12@gmail.com>
2023-10-05 12:40:34 +0530

    first commit

--- Python/Harish/temp.py
+++ Python/Harish/temp.py
@@ -1,8 +1,27 @@
-str1=input("enter a str: ")
-newstr=""
-for i in str1:
-    if i ==" ":
-        pass
-    else:
-        newstr+=i
-print(newstr)
+def tri(n):
+    p=1
+    for i in range(n):
+       
+        for j in range(i+1):
+            print(p, end=" ")
+        p=p+1
+        for j in range(i,n):
+            print(" ", end=" ")
+        for j in range(n,i,-1):
+            print(" ", end=" ")
+        
+        print()
+    p=4
+    for i in range(n+1):
+        
+        for j in range(i,n-1):
+            print(p, end=" ")
+        p=p-1
+        for j in range(i+1):
+            print(" ", end=" ")
+        for j in range(i+1):
+            print(" ", end=" ")
+    
+        
+        print()
+tri(5)
