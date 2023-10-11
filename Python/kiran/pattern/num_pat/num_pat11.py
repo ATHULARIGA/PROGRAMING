b36e1f2bbf3bb6421bfe0107ada37c1c909123bd
ATHULARIGA <athulariga12@gmail.com>
2023-10-11 10:49:12 +0530

    comment

--- Python/kiran/pattern/num_pat/num_pat11.py
+++ Python/kiran/pattern/num_pat/num_pat11.py
@@ -1,16 +1,14 @@
 def hollo_sq(n):
     
     for i in range(n):
-        p=1
+        p=0
         
         for j in range(n):
             if  j==0 or i==0 or i==4 or j==4:
-                if p<=4:
-                    print(p, end=" ")
-                else:
-                    print(j-1,end=" ") 
+                    print(p+1, end=" ")
+                    p=p+1
+
             else:
                 print(" ", end=" ")
-            p+=1
         print()
 hollo_sq(5)
