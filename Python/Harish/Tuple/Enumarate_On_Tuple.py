c48f31540a77c22a60dbbf36c885622fd88dd3e8
ATHULARIGA <athulariga12@gmail.com>
2023-10-16 23:24:35 +0530

    comment

---
+++ Python/Harish/Tuple/Enumarate_On_Tuple.py
@@ -0,0 +1,7 @@
+t=('python',500,50.6)
+for index,data in enumerate(t):
+    print(index,data)
+
+# 0 python
+# 1 500
+# 2 50.6
---
+++ Python/Harish/Tuple/Tup_Type.py
@@ -0,0 +1,15 @@
+#syntax :
+# tuple_Name=(value1,value2,....,valuen)
+t=(10,20,30)
+t1=("rama",30,6.5)
+print(type(t1[2])) # type float
+print("---------------------")
+
+t=(10,20,30,50,60)
+print(type(t))
+
+t=(1)
+print(type(t)) # type int
+
+t=(1,)
+print(type(t)) #type tuple
