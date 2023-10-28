cda950a2a08f6495d1538ca32ff2286ce3d88210
ATHULARIGA <athulariga12@gmail.com>
2023-10-28 10:29:31 +0530

    comment

--- Python/Harish/File_Handling/tell( )_&_seek( ).py
+++ Python/Harish/File_Handling/tell( )_&_seek( ).py
@@ -1,3 +1,9 @@
+# The `seek()` function is used to change the current position of the file pointer in a file. It takes
+# an optional argument that specifies the position to seek to, relative to the beginning of the file.
+
+# The `tell()` function is used to determine the current position of the file pointer in a file. It
+# returns the current position as an integer value.
+
 fptr=open("names.txt", "r")
 pos=fptr.tell()
 print(pos)    #0
