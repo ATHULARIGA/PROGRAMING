1565f1fcb3c612f8f1d698bc3de6e9570c16b2ff
ATHULARIGA <athulariga12@gmail.com>
2023-11-03 10:50:00 +0530

    comment

---
+++ Python/Harish/de_serialisation.py
@@ -0,0 +1,14 @@
+import pickle
+class Employee:
+    def __init__(self, name,id,sal,mob):
+        self.ename = name
+        self.eid = id
+        self.esal=sal
+        self.emobl=mob
+    def disp(self):
+        print(self.ename,self.eid,self.esal,self.emobl)
+e=Employee("rama",101,50000,1111)
+f=open("name.txt","rb")
+e=pickle.load(f)
+
+e.disp()
